c2ef4d71512f2b67719a6ce5c5fb24c314627215
PrinceJetro <adegbuyijephthah@gmail.com>
2024-03-23 07:10:42 -0700

    sylva's changes

--- src/data/video_data.js
+++ src/data/video_data.js
@@ -1,45 +1,24 @@
 import goofy from "../images/video/goofy.jpg";
 import underrated from "../images/video/underated funny moments.jpg";
+import video1 from "../videos/Funniest_Avatar_the_Last_Airbender_Moments(480p).mp4"
+import video2 from "../videos/sokka.mp4"
+import video3 from "../videos/top10.mp4"
+
 
 export default[
     {
         title: "Goofy Moments",
-        img: goofy,
-        link:"https://www.youtube.com/embed/9EXdomrJLcE"
+        img: "https://i.ytimg.com/vi/dGUS4iH6h4s/maxresdefault.jpg",
+        link:video1
     },
     {
         title: "Underrated Funny Moments",
-        img: underrated,
-        link:"https://www.youtube.com/embed/5ujoeaV6Dzk"
+        img: "https://www.watchmojo.com/uploads/thumbs720/MM-TV-Top10-Funniest-Sokka-Moments-on-Avatar-The-Last-Airbender_K4B6E6-f.jpg",
+        link:video2
     },
     {
         title: "three",
-        img: goofy,
-        link:"https://www.youtube.com/embed/9EXdomrJLcE"
-    },
-    {
-        title: "",
-        img: underrated,
-        link:"https://www.youtube.com/embed/5ujoeaV6Dzk"
-    },
-    {
-        title: "",
-        img: goofy,
-        link:"https://www.youtube.com/embed/9EXdomrJLcE"
-    },
-    {
-        title: "",
-        img: underrated,
-        link:"https://www.youtube.com/embed/5ujoeaV6Dzk"
-    },
-    {
-        title: "",
-        img: goofy,
-        link:"https://www.youtube.com/embed/9EXdomrJLcE"
-    },
-    {
-        title: "",
-        img: underrated,
-        link:"https://www.youtube.com/embed/5ujoeaV6Dzk"
+        img: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/07/Zuko-and-Aang-from-Avatar-the-Last-Airbender-Cropped.jpg",
+        link:video3
     },
 ]
